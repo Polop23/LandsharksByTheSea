6e48bc738be9b72279b4184c103d36d482f4eb52
Anton <anton_sparby@live.com>
2017-02-24 13:41:20 +0800

    to annoy mimi

--- DoNotDrownTheRobot/src/org/usfirst/frc/team8621/robot/commands/DriveAndTurnAuto.java
+++ DoNotDrownTheRobot/src/org/usfirst/frc/team8621/robot/commands/DriveAndTurnAuto.java
@@ -25,7 +25,7 @@ public class DriveAndTurnAuto extends CommandGroup {
         //      addSequential(new Command2());
         // Command1 and Command2 will run in parallel.
 
-        // A command group will require all of the subsystems that each member
+        // A command group will require all   of the subsystems that each member
         // would require.
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
